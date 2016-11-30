292bd9ce25e506fed160019b373ddc4a76454801
pc\David <chulded@gmail.com>
2016-11-29 23:19:24 -0500

    Aumento del programa, llamada de funciones

--- lecturadeber.py
+++ lecturadeber.py
@@ -1,3 +1,9 @@
+## EPN-ESFOT-ASI Programacion Avanzada 2016-B
+## Programa Que mide la Frecuencia de una palabra en un archivo de Texto
+
+## Autores: David  Chulde, Danilo Benavides
+## Fecha: 29/11/2016
+
 palabra = input("palabra a buscar ?? ")
 archivo = input("Archivo donde Buscar >> ")
 def creartxt():
@@ -13,6 +19,11 @@ def creartxt():
             if p==palabra:
                 repetidas=repetidas+1
     print("la palabra \"{0}\" se repite {1} veces en el Archivo {2}".format(palabra,repetidas,archivo))
-
+##    print(repetidas)
+    conP.write(str(repetidas))
+    archF.close()
+    conP.close()
 creartxt()
 
+## bibliografia:
+##  http://pharalax.com/blog/python-buscador-de-palabras-en-un-archivo/
