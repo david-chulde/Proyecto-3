6f3b373a5b88d58e0290add4ba228ac92528eeac
HP-2230S-PC\HP-2230S <www.djdanilo@hotmail.com>
2016-11-29 23:08:49 -0500

    Deber busca cuantas veces se repite la palabra Harry

---
+++ lecturadeber.py
@@ -0,0 +1,18 @@
+palabra = input("palabra a buscar ?? ")
+archivo = input("Archivo donde Buscar >> ")
+def creartxt():
+##archivo vacio a quien se va a llenar o escribir write
+    repetidas=0
+    conP=open('deber2.txt','w')
+##archivo lleno con texto se le va a leer read
+    archF=open('harry.txt','r')
+    lines=archF.readlines()
+    for line in lines:
+        palabras=line.split(' ')
+        for p in palabras:
+            if p==palabra:
+                repetidas=repetidas+1
+    print("la palabra \"{0}\" se repite {1} veces en el Archivo {2}".format(palabra,repetidas,archivo))
+
+creartxt()
+
